5192c072366737c50ca6c81261899f0ca921e4f1
Jeison Pereira de Oliveira <jeison.po09@gmail.com>
2023-06-03 10:56:35 -0400

    Aula 4

--- expTS/src/index.ts
+++ expTS/src/index.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import logger from './middlewares/logger';
 import router from './router/router';
 import { engine } from 'express-handlebars';
+import sass from 'node-sass-middleware';
 
 dotenv.config();
 validateEnv();
@@ -11,6 +12,7 @@ validateEnv();
 const PORT = process.env.PORT ?? 3333;
 const app = express();
 const publicPath = `${process.cwd()}/public`;
+const rootPath = process.cwd();
 
 app.engine(
   'handlebars',
@@ -21,6 +23,15 @@ app.engine(
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/views`);
 
+app.use(
+  sass({
+    src: `${publicPath}/scss`,
+    dest: `${publicPath}/css`,
+    outputStyle: 'compressed',
+    prefix: '/css',
+  }),
+);
+
 console.log(process.cwd());
 
 app.use(logger('completo'));
@@ -29,9 +40,19 @@ app.use(router);
 
 app.use('/css', express.static(`${publicPath}/css`));
 
-app.use('/js', express.static(`${publicPath}/js`));
+app.use(
+  '/js',
+  express.static(`${publicPath}/js`),
+  express.static(`${__dirname}/../node_modules/bootstrap/dist/js`),
+);
 
 app.use('/img', express.static(`${publicPath}/img`));
+app.use(
+  '/webfonts',
+  express.static(
+    `${__dirname}/../node_modules/@fortawesome/fontawesome-free/webfonts`,
+  ),
+);
 
 app.listen(PORT, () => {
   console.log(`Servidor escutando na porta ${PORT}`);
